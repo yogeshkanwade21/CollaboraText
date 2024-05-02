cd106fcf254283a2b29368177345632928469bdc
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-05-02 18:59:05 +0530

    add: editor styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import {React, StrictMode} from 'react';
 import {ReactDOM, createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import './styles.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
